fbb217132bca258ac183d3a22d124d38ea8a8978
regular tree <ShinachiAurasa@gmail.com>
2025-02-08 03:22:20 +0200

    Portfolio site v2
    Portfolio site v2
    
    better gallery
    better experience visual
    mobile support (a bit)

--- script.js
+++ script.js
@@ -29,7 +29,40 @@ function openTab(e, tabname) {
     }
 }
 
-var quadimages = document.querySelectorAll("#quad figure");
-for(i=0; i<quadimages.length; i++) {
-  quadimages[i].addEventListener('click', function(){ this.classList.toggle("expanded"); quad.classList.toggle("full") }); 
+let midVideo = document.getElementById('mid-video');
+let midImage = document.getElementById('mid-image');
+
+let centerImage = document.getElementById('center-image');
+let centerText = document.getElementById('center-text');
+
+var images = document.querySelectorAll(".images img");
+
+for(i=0; i<images.length; i++) {
+    images[i].addEventListener('click', function(){
+        centerImage.src = this.getAttribute('src');
+        centerText.innerHTML = this.getAttribute('hiddentext');
+
+        if (!midVideo.classList.contains('mid-hidden')) {
+            midVideo.classList.toggle('mid-hidden');
+            midVideo.src = "";
+        }
+        if (midImage.classList.contains('mid-hidden')) {
+            midImage.classList.toggle('mid-hidden');
+        }
+    }); 
+}
+
+var videos = document.querySelectorAll(".videos img");
+
+for(i=0; i<videos.length; i++) {
+    videos[i].addEventListener('click', function(){
+        midVideo.src = this.getAttribute('hiddenlink');
+
+        if (midVideo.classList.contains('mid-hidden')) {
+            midVideo.classList.toggle('mid-hidden');
+        }
+        if (!midImage.classList.contains('mid-hidden')) {
+            midImage.classList.toggle('mid-hidden');
+        }
+    }); 
 }
